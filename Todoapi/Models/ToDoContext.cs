ede72ac42d2a45bfbf75ae7af19da32e49f4fce4
Gokulnath <84782533+Gsvd123@users.noreply.github.com>
2026-05-11 08:03:43 +0000

    Initial API commit

---
+++ Todoapi/Models/ToDoContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Todoapi.Models
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ToDoItem> ToDoItems { get; set; } = null!;
+    }
+}
---
+++ Todoapi/Models/ToDoItemDTO.cs
@@ -0,0 +1,8 @@
+namespace Todoapi.Models{
+public class ToDoItemDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsComplete { get; set; }
+}
+}
